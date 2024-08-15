19a55c0433a401fca96e9251ff4167a7e9bb1123
Jonas Scholz <jonas.scholz123@gmail.com>
2024-08-15 18:53:45 +0100

    Add black as formatter

--- src/exploration/interpolation.py
+++ src/exploration/interpolation.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import deepsensor.torch
 from deepsensor.data.loader import TaskLoader
 from deepsensor.data.processor import DataProcessor
@@ -14,98 +14,146 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cf
 import seaborn as sns
+
 # %%
 crs = ccrs.PlateCarree()
 
 use_gpu = True
 if use_gpu:
     set_gpu_default_device()
-#%%
+# %%
 
 elevation_path = "../../_data/elevation/elevation.nc"
-hires_aux_raw_ds = xr.open_mfdataset(elevation_path).coarsen(lat=5, lon=5, boundary='trim').mean()
+hires_aux_raw_ds = (
+    xr.open_mfdataset(elevation_path).coarsen(lat=5, lon=5, boundary="trim").mean()
+)
 hires_aux_raw_ds = hires_aux_raw_ds
 print(hires_aux_raw_ds)
-#%%
+# %%
 print(hires_aux_raw_ds)
 fig, ax = plt.subplots(1, 1, subplot_kw=dict(projection=crs))
-hires_aux_raw_ds['height'].plot(ax=ax)
+hires_aux_raw_ds["height"].plot(ax=ax)
 ax.add_feature(cf.BORDERS)
 ax.coastlines()
-#%%
-aux_raw_ds = hires_aux_raw_ds.coarsen(lat=20, lon=20, boundary='trim').mean()["height"]
+# %%
+aux_raw_ds = hires_aux_raw_ds.coarsen(lat=20, lon=20, boundary="trim").mean()["height"]
 fig, ax = plt.subplots(1, 1, subplot_kw=dict(projection=crs))
 aux_raw_ds.plot(ax=ax)
 ax.add_feature(cf.BORDERS)
 ax.coastlines()
 print(aux_raw_ds)
-#%%
+# %%
 # Print resolution of lowres and hires elevation data
-print(f"Lowres lat resolution: {np.abs(np.diff(aux_raw_ds.coords['lat'].values)[0]):.4f} degrees")
-print(f"Lowres lon resolution: {np.abs(np.diff(aux_raw_ds.coords['lon'].values)[0]):.4f} degrees")
-print(f"Hires lat resolution: {np.abs(np.diff(hires_aux_raw_ds.coords['lat'].values)[0]):.4f} degrees")
-print(f"Hires lon resolution: {np.abs(np.diff(hires_aux_raw_ds.coords['lon'].values)[0]):.4f} degrees")
-#%%
+print(
+    f"Lowres lat resolution: {np.abs(np.diff(aux_raw_ds.coords['lat'].values)[0]):.4f} degrees"
+)
+print(
+    f"Lowres lon resolution: {np.abs(np.diff(aux_raw_ds.coords['lon'].values)[0]):.4f} degrees"
+)
+print(
+    f"Hires lat resolution: {np.abs(np.diff(hires_aux_raw_ds.coords['lat'].values)[0]):.4f} degrees"
+)
+print(
+    f"Hires lon resolution: {np.abs(np.diff(hires_aux_raw_ds.coords['lon'].values)[0]):.4f} degrees"
+)
+# %%
 from scaffolding_v3.data.dwd import DwdDataProvider
 from scaffolding_v3.config import Paths
 
 ppu = 150
 dwd_data_provider = DwdDataProvider(Paths(), 500, 1000, 0.1, ppu)
-#%%
-station_raw_df = dwd_data_provider.df
+# %%
+station_raw_df = dwd_data_provider.train_df
 station_raw_df = station_raw_df.reset_index()
-station_raw_df['time'] = pd.to_datetime(station_raw_df['time'])
+station_raw_df["time"] = pd.to_datetime(station_raw_df["time"])
 station_raw_df = station_raw_df.set_index(["time", "lat", "lon"])
 station_raw_df = station_raw_df[["t2m"]]
-#%%
-data_processor = DataProcessor(x1_name="lat", x1_map=(aux_raw_ds["lat"].min(), aux_raw_ds["lat"].max()), x2_name="lon", x2_map=(aux_raw_ds["lon"].min(), aux_raw_ds["lon"].max()))
+# %%
+data_processor = DataProcessor(
+    x1_name="lat",
+    x1_map=(aux_raw_ds["lat"].min(), aux_raw_ds["lat"].max()),
+    x2_name="lon",
+    x2_map=(aux_raw_ds["lon"].min(), aux_raw_ds["lon"].max()),
+)
 station_df = data_processor([station_raw_df])[0]
 aux_ds, hires_aux_ds = data_processor([aux_raw_ds, hires_aux_raw_ds], method="min_max")
 print(data_processor)
-#%%
-task_loader = TaskLoader(context=[station_df, aux_ds], target=station_df, aux_at_targets=hires_aux_ds, links=[(0, 0)])
+# %%
+task_loader = TaskLoader(
+    context=[station_df, aux_ds],
+    target=station_df,
+    aux_at_targets=hires_aux_ds,
+    links=[(0, 0)],
+)
 task_loader.load_dask()
 
 print(task_loader)
-#%%
-model = ConvNP(data_processor, task_loader, unet_channels=(64,)*4, internal_density=ppu, likelihood="cnp")
-#%%
+# %%
+model = ConvNP(
+    data_processor,
+    task_loader,
+    unet_channels=(64,) * 4,
+    internal_density=ppu,
+    likelihood="cnp",
+)
+# %%
 times = station_df.index.get_level_values("time").unique()
-train_times = times[:int(len(times)*0.8)][::5]
-val_times = times[int(len(times)*0.8):][::5]
-#%%
+train_times = times[: int(len(times) * 0.8)][::5]
+val_times = times[int(len(times) * 0.8) :][::5]
+# %%
 from tqdm import tqdm
+
 train_tasks = []
 for time in tqdm(train_times):
     split_frac = np.random.uniform(0.0, 1.0)
-    task = task_loader(time, context_sampling=["split", "all"], target_sampling="split", split_frac=split_frac)
+    task = task_loader(
+        time,
+        context_sampling=["split", "all"],
+        target_sampling="split",
+        split_frac=split_frac,
+    )
     train_tasks.append(task)
-#%%
+# %%
 val_tasks = []
 for time in tqdm(val_times):
     split_frac = np.random.uniform(0.0, 1.0)
-    task = task_loader(time, context_sampling=["split", "all"], target_sampling="split", split_frac=split_frac)
+    task = task_loader(
+        time,
+        context_sampling=["split", "all"],
+        target_sampling="split",
+        split_frac=split_frac,
+    )
     val_tasks.append(task)
-#%%
+# %%
 train_tasks[0]
-#%%
+# %%
 fig = deepsensor.plot.context_encoding(model, train_tasks[0], task_loader)
-#%%
+# %%
 fig, ax = plt.subplots(1, 1, figsize=(7, 7), subplot_kw=dict(projection=crs))
 ax.coastlines()
 ax.add_feature(cf.BORDERS)
-deepsensor.plot.offgrid_context(ax, train_tasks[4], data_processor, task_loader, plot_target=True, add_legend=True, linewidths=0.5)
+deepsensor.plot.offgrid_context(
+    ax,
+    train_tasks[4],
+    data_processor,
+    task_loader,
+    plot_target=True,
+    add_legend=True,
+    linewidths=0.5,
+)
 plt.show()
-#%%
+# %%
 from scaffolding_v3.config import Paths
 import lab as B
 from tqdm import tqdm
+
 
 def compute_val_loss(model, val_tasks):
     val_losses = []
     for task in val_tasks:
         val_losses.append(B.to_numpy(model.loss_fn(task, normalise=True)))
     return np.mean(val_losses)
+
 
 n_epochs = 80
 train_losses = []
@@ -124,7 +172,7 @@ for epoch in tqdm(range(n_epochs)):
     print("Epoch: ", epoch)
     print("Val loss: ", val_loss)
     print("Train loss: ", train_loss)
-#%%
+# %%
 fig, ax = plt.subplots(1, 1, figsize=(4, 4))
 ax.plot(train_losses, label="train")
 ax.plot(val_losses, label="val")
--- src/scaffolding_v3/evaluate.py
+++ src/scaffolding_v3/evaluate.py
@@ -1,49 +1,73 @@
-#%%
+# %%
 import hydra
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cf
+import deepsensor.torch
 import deepsensor.plot
 from hydra.core.config_store import ConfigStore
 from mlbnb.paths import config_to_filepath
 from mlbnb.checkpoint import CheckpointManager
 from omegaconf import DictConfig
-from pathlib import Path
 
 from scaffolding_v3.data.elevation import load_elevation_data
-from scaffolding_v3.data.dwd import DwdDataProvider
-from scaffolding_v3.config import Paths, Config, ExecutionConfig, OutputConfig, SKIP_KEYS
-
+from scaffolding_v3.data.dwd import get_data_processor
+from scaffolding_v3.config import (
+    Paths,
+    Config,
+    ExecutionConfig,
+    OutputConfig,
+    SKIP_KEYS,
+)
 
 
 fontsize = 14
 crs = ccrs.PlateCarree()
 
 params = {
-    'axes.labelsize': fontsize,
-    'axes.titlesize': fontsize,
-    'font.size': fontsize,
-    'figure.titlesize': fontsize,
-    'legend.fontsize': fontsize,
-    'xtick.labelsize': fontsize,
-    'ytick.labelsize': fontsize,
-    'font.family': 'sans-serif',
-    'figure.facecolor': 'w',
+    "axes.labelsize": fontsize,
+    "axes.titlesize": fontsize,
+    "font.size": fontsize,
+    "figure.titlesize": fontsize,
+    "legend.fontsize": fontsize,
+    "xtick.labelsize": fontsize,
+    "ytick.labelsize": fontsize,
+    "font.family": "sans-serif",
+    "figure.facecolor": "w",
 }
 
 import matplotlib as mpl
+
 mpl.rcParams.update(params)
 
 cs = ConfigStore.instance()
 cs.store(name="dev", node=Config)
-cs.store(name="prod", node=Config(
-    execution=ExecutionConfig(dry_run=False),
-    output=OutputConfig(use_wandb=True)
-    ))
+cs.store(
+    name="prod",
+    node=Config(
+        execution=ExecutionConfig(dry_run=False), output=OutputConfig(use_wandb=True)
+    ),
+)
 
 
-def gen_test_fig(era5_raw_ds=None, mean_ds=None, std_ds=None, samples_ds=None, task=None, extent=None, add_colorbar=False, var_cmap="jet", var_clim=None, std_cmap="Greys", std_clim=None, var_cbar_label=None, std_cbar_label=None, fontsize=None, figsize=(15, 5)):
+def gen_test_fig(
+    era5_raw_ds=None,
+    mean_ds=None,
+    std_ds=None,
+    samples_ds=None,
+    task=None,
+    extent=None,
+    add_colorbar=False,
+    var_cmap="jet",
+    var_clim=None,
+    std_cmap="Greys",
+    std_clim=None,
+    var_cbar_label=None,
+    std_cbar_label=None,
+    fontsize=None,
+    figsize=(15, 5),
+):
     if var_clim is None and era5_raw_ds is not None and mean_ds is not None:
         vmin = np.array(min(era5_raw_ds.min(), mean_ds.min()))
         vmax = np.array(max(era5_raw_ds.max(), mean_ds.max()))
@@ -78,26 +102,54 @@ def gen_test_fig(era5_raw_ds=None, mean_ds=None, std_ds=None, samples_ds=None, t
     if era5_raw_ds is not None:
         ax = axes[axis_i]
         # era5_raw_ds.sel(lat=slice(mean_ds["lat"].min(), mean_ds["lat"].max()), lon=slice(mean_ds["lon"].min(), mean_ds["lon"].max())).plot(ax=ax, cmap="jet", vmin=vmin, vmax=vmax, add_colorbar=False)
-        era5_raw_ds.plot(ax=ax, cmap=var_cmap, vmin=vmin, vmax=vmax, add_colorbar=add_colorbar, cbar_kwargs=dict(label=var_cbar_label))
+        era5_raw_ds.plot(
+            ax=ax,
+            cmap=var_cmap,
+            vmin=vmin,
+            vmax=vmax,
+            add_colorbar=add_colorbar,
+            cbar_kwargs=dict(label=var_cbar_label),
+        )
         ax.set_title("ERA5", fontsize=fontsize)
         axis_i += 1
 
     if mean_ds is not None:
         ax = axes[axis_i]
-        mean_ds.plot(ax=ax, cmap=var_cmap, vmin=vmin, vmax=vmax, add_colorbar=add_colorbar, cbar_kwargs=dict(label=var_cbar_label))
+        mean_ds.plot(
+            ax=ax,
+            cmap=var_cmap,
+            vmin=vmin,
+            vmax=vmax,
+            add_colorbar=add_colorbar,
+            cbar_kwargs=dict(label=var_cbar_label),
+        )
         ax.set_title("ConvNP mean", fontsize=fontsize)
         axis_i += 1
 
     if samples_ds is not None:
         for i in range(samples_ds.shape[0]):
             ax = axes[axis_i]
-            samples_ds.isel(sample=i).plot(ax=ax, cmap=var_cmap, vmin=vmin, vmax=vmax, add_colorbar=add_colorbar, cbar_kwargs=dict(label=var_cbar_label))
+            samples_ds.isel(sample=i).plot(
+                ax=ax,
+                cmap=var_cmap,
+                vmin=vmin,
+                vmax=vmax,
+                add_colorbar=add_colorbar,
+                cbar_kwargs=dict(label=var_cbar_label),
+            )
             ax.set_title(f"ConvNP sample {i+1}", fontsize=fontsize)
             axis_i += 1
 
     if std_ds is not None:
         ax = axes[axis_i]
-        std_ds.plot(ax=ax, cmap=std_cmap, add_colorbar=add_colorbar, vmin=std_vmin, vmax=std_vmax, cbar_kwargs=dict(label=std_cbar_label))
+        std_ds.plot(
+            ax=ax,
+            cmap=std_cmap,
+            add_colorbar=add_colorbar,
+            vmin=std_vmin,
+            vmax=std_vmax,
+            cbar_kwargs=dict(label=std_cbar_label),
+        )
         ax.set_title("ConvNP std dev", fontsize=fontsize)
         axis_i += 1
 
@@ -108,34 +160,34 @@ def gen_test_fig(era5_raw_ds=None, mean_ds=None, std_ds=None, samples_ds=None, t
             ax.set_extent(extent)
     if task is not None:
         deepsensor.plot.offgrid_context(axes, task, data_processor, task_loader)
-    print("Done")
     return fig, axes
 
 
-cfg = DictConfig(Config(
-    execution=ExecutionConfig(dry_run=False),
-    output=OutputConfig(use_wandb=True),
-))
+cfg = DictConfig(
+    Config(
+        execution=ExecutionConfig(dry_run=False),
+        output=OutputConfig(use_wandb=True),
+    )
+)
 paths = Paths()
 
+data_processor = get_data_processor(paths)
+
 data_provider = hydra.utils.instantiate(cfg.data.data_provider)
-trainset = data_provider.get_train_data()
-valset = data_provider.get_val_data()
+
 collate_fn = data_provider.get_collate_fn()
 
-model = hydra.utils.instantiate(
-    cfg.model, trainset.data_processor, trainset.task_loader
-)
+test_dataset = data_provider.get_test_data()
+task_loader = test_dataset.task_loader
+
+model = hydra.utils.instantiate(cfg.model, data_processor, task_loader)
 
 path = config_to_filepath(cfg, cfg.output.out_dir, SKIP_KEYS)
 checkpoint_manager = CheckpointManager(path)
 
-test_dataset = data_provider.get_test_data()
-data_processor = test_dataset.data_processor
-task_loader = test_dataset.task_loader
 hires_aux_raw_ds = load_elevation_data(paths, 500)
 
-#%%
+# %%
 
 model = hydra.utils.instantiate(cfg.model, data_processor, task_loader)
 checkpoint = checkpoint_manager.load_checkpoint("best")
@@ -147,7 +199,9 @@ for context_sampling in [20, 100, "all"]:
         context_sampling=[context_sampling, "all"],
         target_sampling="all",
     )
-    pred = model.predict(test_task, X_t=hires_aux_raw_ds.sel(lat=slice(55, 47.5), lon=slice(6, 15)))["t2m"]
+    pred = model.predict(
+        test_task, X_t=hires_aux_raw_ds.sel(lat=slice(55, 47.5), lon=slice(6, 15))
+    )["t2m"]
 
     mean_ds, std_ds = pred["mean"], pred["std"]
 
@@ -161,8 +215,8 @@ for context_sampling in [20, 100, "all"]:
         var_cbar_label="2m temperature [°C]",
         std_cbar_label="std dev [°C]",
         std_clim=(1, 3),
-        var_clim=(-4., 10.),
+        var_clim=(-4.0, 10.0),
         extent=(6, 15, 47.5, 55),
-        figsize=(20, 20/3)
+        figsize=(20, 20 / 3),
     )
-#%%
+# %%
