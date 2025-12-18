79ee718ab41ad7f91eb3f2f7ec7504e9f2a1c285
Jonas Scholz <jonas.scholz123@gmail.com>
2025-12-18 19:44:25 +0000

    Simplify test

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,6 @@
 """Checks that determinstic config initializes as expected."""
 
-import hashlib
-
 import pytest
-import torch
 
 from scaffolding_v3.train import Trainer, train_loop
 from scaffolding_v3.util.instantiate import Experiment, load_config
@@ -30,14 +27,6 @@ def test_trainer_initialises(mode: str, data: str) -> None:
     _ = Trainer.from_experiment(exp, cfg)
 
 
-def get_model_checksum(model: torch.nn.Module) -> str:
-    """Computes a checksum over the model parameters."""
-    hasher = hashlib.sha256()
-    for param in model.parameters():
-        hasher.update(param.detach().cpu().numpy().tobytes())
-    return hasher.hexdigest()
-
-
 def test_training_determinism() -> None:
     """Checks that training is deterministic by comparing model checksums."""
     overrides = [
@@ -50,14 +39,12 @@ def test_training_determinism() -> None:
 
     # Run 1
     cfg1 = load_config(overrides=overrides, config_path="../../config")
-    train_loop(cfg1)
-    exp1 = Experiment.from_config(cfg1, checkpoint="latest")
-    checksum1 = get_model_checksum(exp1.model)
+    val_loss1 = train_loop(cfg1)
 
     # Run 2
     cfg2 = load_config(overrides=overrides, config_path="../../config")
-    train_loop(cfg2)
-    exp2 = Experiment.from_config(cfg2, checkpoint="latest")
-    checksum2 = get_model_checksum(exp2.model)
+    val_loss2 = train_loop(cfg2)
 
-    assert checksum1 == checksum2, f"Non-deterministic training! {checksum1} != {checksum2}"
+    assert val_loss1 == val_loss2, (
+        f"Non-deterministic training! {val_loss1} != {val_loss2}"
+    )
